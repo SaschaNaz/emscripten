dde3ff8cb404ed29079813727a090395353a99f3
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-12-21 16:56:01 +0900

    second try

---
+++ tools/force_python_version.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+def run(filename):
+  major = sys.version_info.major
+  if major == 2:
+    return False
+  if os.environ.get("EMSCRIPTEN_ALLOW_NEWER_PYTHON"):
+    import logging
+    # TODO: colored log
+    logging.warning("Running on Python %s which is not officially supported yet" % major)
+    return False
+  import subprocess
+  sys.exit(subprocess.call(['python2', os.path.realpath(filename) + '.py'] + sys.argv[1:]))
+  return True
